7dcb20dd875c282fcd3c370bda516f58569fcd2c
cfossguy <cfossguy@gmail.com>
2024-06-11 18:27:27 -0400

    Initial commit of data pipeline code for workshop. "scrape", "upsert", "delete" for cnn.com web scrape. Hard way(no canopy).

--- data/README.md
+++ data/README.md
@@ -1,2 +1,59 @@
-# data
-This folder contains data required for the use cases
+# Workshop Data Pipeline
+Quickstart guide for running the data pipeline for a Pinecone workshop. The end to end pipeline does the following:
+
+1. Scrapes web content from a base URL.
+1. Creates JSONL files that contain the text for each web scrape and key meta-data fields like "source" and "scrape_date"
+1. Crease JSONL data files that contain the text chunks for each web scrape, vector representation for each chunk and key meta-data fields like "source" and "scrape_date"
+1. Upserts the JSONL data files into Pinecone
+
+### Step 1 - Install dependencies
+
+Setup virtual environment and install the required python packages. If you do not have poetry, you will need to install
+with this command:
+
+```
+cd ./data
+pip install poetry
+poetry install
+poetry shell
+```
+
+### Step 2 - Set environment variables
+Create a file named ```.env``` that has the following variables:
+
+```
+PINECONE_NAMESPACE=cnn
+PINECONE_INDEX_NAME=workshop-[YOUR_NAME]
+PINECONE_API_KEY=[YOUR_PINECONE_API_KEY]
+```
+
+### Step 3 - Run data pipeline - web scrape
+
+```
+python data_pipeline.py scrape
+```
+
+### Step 4 - Run data pipeline - pinecone upsert
+
+```
+python data_pipeline.py upsert
+```
+
+### Step 5 - Run a test query in Pinecone console
+
+1. Login to the pinecone console
+1. Select your workshop index
+1. Click "Browser"
+1. Enter your namespace in the "Namespace" field
+1. Copy a vector from your JSONL data file (one of the "values" entries)
+1. Paste the vector value in the "vector" field
+1. Click "Query" button
+1. Validate that the "text" metadata field in the first entry matches the "text" entry in your JSONL data file
+
+### Step 6 - Run data pipeline - pinecone delete
+
+This command will delete the vectors in your namespace. It will NOT delete the JSONL data that you web scraped. 
+
+```
+python data_pipeline.py upsert
+```
---
+++ data/data_pipeline.py
@@ -0,0 +1,139 @@
+import json
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import argparse
+import time
+import requests
+import random
+from datetime import date
+import os
+from pinecone import Pinecone
+from transformers import AutoModel, AutoTokenizer
+from pinecone import Pinecone
+import pandas as pd
+
+load_dotenv()
+
+API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
+PINECONE_NAMESPACE = os.getenv("PINECONE_NAMESPACE")
+
+# Commented out some sections to reduce the scrape time
+# news_sections = ["us", "world", "politics", "business", "health", "entertainment", "style", "travel", "sports"]
+news_sections = ["us", "world", "politics"]
+
+def get_article_urls(section):
+    articles = []
+    response = requests.get(f"https://www.cnn.com/{section}")
+
+    # Parse the HTML content
+    soup = BeautifulSoup(response.content, 'html.parser')
+    links = soup.select('a[data-link-type="article"]')
+    site_prefix = "http://cnn.com"
+    for link in links:
+        articles.append(f"{site_prefix}{link['href']}")
+
+    articles_no_duplicates = list(set(articles))
+
+    return list(articles_no_duplicates)
+
+def get_article_details(urls):
+    details = []
+    #for url in urls:
+    for url in urls:
+        try:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            script_tag = soup.find('script', {'type': 'application/ld+json'})
+            data = json.loads(script_tag.string)
+            text = data['articleBody']
+            details.append({"url": url, "text": text, "scrape_date": date.today().strftime("%m/%d/%Y")})
+            print(f"Web scraped article from {url}")
+            time.sleep(random.uniform(.5, 1))  
+        except Exception as e:
+            print(f"Web scraped article from {url}: {e}")
+    print(f"Web scraped {len(details)} articles")
+    return details
+
+def create_jsonl_file(section, article_details):
+    with open(f'jsonl/cnn_articles_{section}.jsonl', 'w') as f:
+        for article_detail in article_details:
+            for text_embedding in generate_embeddings_from_text(article_detail['text']):
+                doc_id = get_article_id(article_detail['url'])
+                jsonl_element = {}
+                jsonl_element['id'] = f'doc-{doc_id}#chunk{text_embedding['chunk_id']}'
+                jsonl_element['values'] = text_embedding['embedding']
+                jsonl_element['metadata'] = {"text": text_embedding['text'], 
+                                             "scrape_date": article_detail['scrape_date'], 
+                                             "section": section, 
+                                             "source": article_detail['url']}
+               
+                f.write(json.dumps(jsonl_element) + '\n')
+            print(f"Wrote article as doc_id: {doc_id} to jsonl file")
+    print(f"Wrote {len(article_details)} articles to data directory")
+
+def generate_embeddings_from_text(text):
+    
+    model = AutoModel.from_pretrained('intfloat/multilingual-e5-large')
+    tokenizer = AutoTokenizer.from_pretrained('intfloat/multilingual-e5-large')
+    chunk_size = 512
+    overlap = 50
+    text_embeddings = []
+    
+    chunks = [text[i:i+chunk_size] for i in range(0, len(text), chunk_size-overlap)]
+    for chunk in chunks:
+        tokens = tokenizer(chunk, return_tensors='pt', padding=True)
+        outputs = model(**tokens)
+        embedding = outputs.last_hidden_state.mean(dim=1).squeeze().tolist()
+        
+        text_embeddings.append({'text':chunk,
+                                'chunk_id':chunks.index(chunk),
+                                'embedding':embedding })
+    return text_embeddings
+
+def scrape():
+    for section in news_sections:
+        urls = get_article_urls(section)
+        article_details = get_article_details(urls)
+        create_jsonl_file(section, article_details)
+
+def upsert():
+    pc = Pinecone(api_key=API_KEY)
+    index = pc.Index(PINECONE_INDEX_NAME)
+    
+    directory_path = os.path.join(os.path.dirname(__file__), "./jsonl")
+
+    for filename in os.listdir(directory_path):
+        if filename.endswith('.jsonl'):
+            filepath = os.path.join(directory_path, filename)
+            with open(filepath, 'r') as file:
+                df = pd.read_json(file, lines=True)
+                index.upsert_from_dataframe(df, namespace=PINECONE_NAMESPACE)
+
+def delete_data():
+    pc = Pinecone(api_key=API_KEY)
+    index = pc.Index(PINECONE_INDEX_NAME)
+    index.delete(delete_all=True, namespace=PINECONE_NAMESPACE)
+    print(f"Deleted all vectors in index: {PINECONE_INDEX_NAME} for namespace: {PINECONE_NAMESPACE}")
+
+def get_article_id(url):
+    s = url
+    s = s[15:]
+    s = s.replace("/", "_")
+    s = s[:-11]
+    return s
+
+def main():
+    parser = argparse.ArgumentParser(description="CLI for upserting and deleted pinecone index data")
+    parser.add_argument("action", choices=["scrape", "upsert", "delete"], help="Action to perform: 'scrape' to scrape data from base url, 'upsert' to insert or update data, 'delete' to delete all data in namespace")
+    args = parser.parse_args()
+
+    if args.action == "scrape":
+        scrape()
+    elif args.action == "upsert":
+        upsert()
+    elif args.action == "delete":
+        delete_data()
+
+if __name__ == "__main__":
+    main()
